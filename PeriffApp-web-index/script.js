b189120bdd392a2c52e5eed81a221a6ee3acf37b
PeriffApp <leotubee@outlook.com>
2025-05-08 11:32:17 -0300

    implementando o login
    Adicionado um observador do login, para verificar se o usuário esta logado ou deslogado. Adicionado também o botão de Sair para o deslog

--- PeriffApp-web-index/script.js
+++ PeriffApp-web-index/script.js
@@ -1,12 +1,15 @@
-import { auth, signInWithEmailAndPassword } from "./firebase.js"; // Importando o Firebase
+import { auth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "./firebase.js"; // Importando o Firebase
 
 
 // JavaScript para controle dos modais e formulários
 document.addEventListener('DOMContentLoaded', function() {
 
+    
+
     // Elementos do modal
     const modal = document.getElementById('loginModal');
     const btnOpenModal = document.querySelector('.cta-button');
+    const btnLogout = document.getElementById('logoutButton');
     const spanClose = document.querySelector('.close-modal');
     const btnLogin = document.getElementById('btnLogin');
 
@@ -46,30 +49,68 @@ document.addEventListener('DOMContentLoaded', function() {
         
     }
 
+    // observador para verificar se o usuario está logado
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+                      
+            const uid = user.uid;
+            console.log("STATUS: Usuário logado com UID: " + uid);
+            btnOpenModal.style.display = 'none'; // Esconde o botão de login
+            btnLogout.style.display = 'block'; // Mostra o botão de logout
+
+        } else {
+            console.log("STATUS: Usuário não logado");
+        }
+    });
+
     btnLogin.addEventListener('click', e => {
         e.preventDefault();
 
+        // pega os valores dos inputs de email e senha
         const Email = email.value.trim();
         const Password = password.value;
 
-       
+        // realiza o login com o firebase autentication
         signInWithEmailAndPassword(auth, Email, Password)
         .then((userCredential) => {
-          // Signed in 
+          
+          // pega o usuario logado  
           const user = userCredential.user;
-          // ...
+
           alert("Login realizado com sucesso!")
+
           // redirecionar para a pagina perfilPrestador.html
           window.location.replace("./PerfilPrestador/perfilPrestador.html");
+          
 
         })
         .catch((error) => {
           alert("Erro ao realizar login!")
 
         });
-        
-        
     })
+
+    btnLogout.addEventListener('click', e => {
+        e.preventDefault();
+
+        signOut(auth)
+        .then(() => {
+          // Logout bem‑sucedido
+          alert("Você saiu da sua conta.");
+          // Redireciona de volta para a página de login
+          btnLogout.style.display = 'none';
+          btnOpenModal.style.display = 'block';
+        })
+        .catch((error) => {
+          console.error("Erro ao sair:", error);
+          alert("Não foi possível deslogar. Tente novamente.");
+        });
+
+
+
+    })
+
+
     
         
         
